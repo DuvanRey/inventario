cd2e00a5e174a955caebbaa4f5aaa0b00d77ee47
Duvan <duvanfrey13@gmail.com>
2025-04-13 19:04:42 -0500

    JavaScript CRUD

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors"
 import categoriaRoutes from "./routes/categorias.routes.js"
 
 const app = express();
 app.set("port", 5000)  /*setear el puerto*/
+
+app.use(cors());
 
 app.use("/api/categorias", categoriaRoutes)
 export default app; 
